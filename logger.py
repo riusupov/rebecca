beaeff86740b1c755dc68b3f32b373c80feb42e3
Pirmin Vogel <vogelpi@lowrisc.org>
2020-11-05 11:38:14 +0100

    Add .gitignore

--- logger.py
+++ logger.py
@@ -1,4 +1,15 @@
 import logging
+import os
+
+# Make sure the work directory exists.
+path = os.getcwd()
+tmp_dir = path + '/tmp'
+if not os.path.exists(tmp_dir):
+	try:
+		os.makedirs(tmp_dir)
+	except OSError:
+		print('ERR: cannot create dir ' + str(tmp_dir))
+		exit()
 
 logger = logging.getLogger()
 handler = logging.FileHandler('tmp/log.txt')
